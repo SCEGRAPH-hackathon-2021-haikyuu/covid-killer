9b63fa3dcfb266afb720142c6d5e3247cb791637
Dan Le <danhchl1@gmail.com>
2021-01-08 11:53:36 -0800

    Added simple player movement

---
+++ Covid Killer/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform player;
+    public float smooth = 0.3f;
+    public float height;
+    public float distance;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update() {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.z = player.position.z - distance;
+        pos.y = player.position.y + height;
+        this.transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+        this.transform.LookAt(player.position);
+    }
+}
---
+++ Covid Killer/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Start is called before the first frame update
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update() {
+        // make player face mouse
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray rayline = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+
+        if(playerPlane.Raycast(rayline, out hitDist)) {
+            Vector3 targetPoint = rayline.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        //Player Movement
+        if(Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+
+    }
+}
