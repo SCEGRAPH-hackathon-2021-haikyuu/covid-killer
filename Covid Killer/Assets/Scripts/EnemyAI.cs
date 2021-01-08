2a0820331659923c91762e58055b4f2759405589
ASokry <anthonysokry11@gmail.com>
2021-01-08 11:59:17 -0800

    Enemy Follow and face direction

--- Covid Killer/Assets/Scripts/EnemyAI.cs
+++ Covid Killer/Assets/Scripts/EnemyAI.cs
@@ -6,27 +6,70 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent enemy;
-    public Transform player1;
-    public Transform player2;
+    private Rigidbody rb;
+    [SerializeField]private GameObject[] players;
+    public GameObject projectilePrefab;
+    public bool followPlayer = false;
+    public bool shootPlayer = false;
+    public float startTime = 2.0f;
+    private float timeToShoot;
 
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
-        if (GameObject.Find("Player1") != null)
-        {
-            player1 = GameObject.Find("Player1").GetComponent<Transform>();
-        }
-
-        if (GameObject.Find("Player2") != null)
-        {
-            player2 = GameObject.Find("Player2").GetComponent<Transform>();
-        }
+        rb = GetComponent<Rigidbody>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+        timeToShoot = startTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (players.Length > 0)
+        {
+            Transform closestPlayer = GetClosestPlayer(players, transform);
+            Vector3 lookDir = (closestPlayer.position - transform.position).normalized;
+            float angle = Mathf.Atan2(lookDir.x, lookDir.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f,angle,0f);
+
+            if (followPlayer)
+            {
+                enemy.SetDestination(closestPlayer.position);
+            }
+        }
+
+        if (shootPlayer && projectilePrefab != null)
+        {
+            if (timeToShoot <= 0)
+            {
+                Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+                timeToShoot = startTime;
+            }
+            else
+            {
+                timeToShoot -= Time.deltaTime;
+            }
+        }
+    }
+
+    Transform GetClosestPlayer(GameObject[] players, Transform enemy)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = enemy.position;
+        foreach (GameObject potentialTarget in players)
+        {
+            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget.transform;
+            }
+        }
+        return bestTarget;
     }
 }
---
+++ Covid Killer/Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.GetComponent<Rigidbody>() != null)
+            rb = gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+            rb.velocity = transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision);
+    }
+}
